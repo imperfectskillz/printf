da56673d9f671e9dcd02c57410ac76e47c31df71
imperfectskillz <j.choi.89@gmail.com>
2017-10-23 23:15:40 +0000

    basic printf

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ *_printf- copy of printf function
+ *@format: string containing characters
+ *Return: number of chars printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list valist;
+	int i;
+	int j;
+
+	va_start(valist, format);
+
+	op_t ops[] = {
+		{"c", print_c}
+		{"s", print_s}
+		{"%", print_per}
+		{"d", print_d}
+		{"i", print_i}
+	};
+
+	if (format == NULL)
+		return (-1);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			for (j = 0; ops[j].op != '\0'; j++)
+			{
+				if (format[i + 1] == ops[j].op)
+				{
+					_write(ops[j].f(valist));
+				}
+			}
+		}
+		else
+		{
+			_write(format[i]);
+		}
+	}
+}
